29cea2cfc616ea68f34c24a00b82f5db9890d21c
Petr Malik <petr.malik@hotmail.com>
2023-06-03 09:38:41 -0400

    VAN-4172 Load data from multiple sources
    Load data from multiple sample apps.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,23 @@
 module "voting-demo" {
-  source = "./voting-demo"
+  source = "github.com/cldcvr/codepipes-tutorials//voting/infra/aws/eks?ref=terrarium-sources"
+}
+
+module "serverless-sample" {
+  source = "github.com/cldcvr/codepipes-tutorials//serverless-sample/infra/aws?ref=terrarium-sources"
+}
+
+module "wpdemo-eks" {
+  source = "github.com/cldcvr/codepipes-tutorials//wpdemo/infra/aws/eks?ref=terrarium-sources"
+}
+
+module "wpdemo-ec2" {
+  source = "github.com/cldcvr/codepipes-tutorials//wpdemo/infra/aws/ec2?ref=terrarium-sources"
+}
+
+module "cdn" {
+  source = "github.com/cldcvr/vanguard-demo//cdn/infra/aws/eks"
+}
+
+module "codepipes-iac" {
+  source = "github.com/cldcvr/vanguard-iac//modules/vanguard-infra"
 }
