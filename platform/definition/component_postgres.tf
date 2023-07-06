de8302e397bdc053241bf30d212edd53a7bb5dee
Kanak Singhal <kanak@cldcvr.com>
2023-07-06 23:06:04 +0530

    TER-86 terrarium platform definition (#18)
    * TER-86 wip
    
    Co-authored-by: Shwetanshu Mehta <shwetanshu-cldcvr@users.noreply.github.com>
    
    * TER-86 terrarium platform definition
    
    Refer readme.md at: `platform/definition/readme.md`
    
    * metadata improvements
    
    ---------
    
    Co-authored-by: Shwetanshu Mehta <shwetanshu-cldcvr@users.noreply.github.com>

---
+++ platform/definition/component_postgres.tf
@@ -0,0 +1,37 @@
+module "tr_component_postgres" {
+  source = "terraform-aws-modules/rds/aws"
+
+  for_each = local.tr_component_postgres
+
+  identifier     = each.key
+  engine_version = each.value.version
+  db_name        = each.value.db_name
+  engine         = "postgres"
+  family         = format("postgres%s", each.value.version)
+
+  instance_class = coalesce(lookup(var.db_instance_class, each.key, null), var.all_db_instance_class)
+
+  vpc_security_group_ids = [module.postgres_security_group.security_group_id]
+  subnet_ids             = module.core_vpc.database_subnets
+}
+
+module "postgres_security_group" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name   = "postgres_sg"
+  vpc_id = module.core_vpc.vpc_id
+
+  # ingress
+  ingress_with_cidr_blocks = [
+    {
+      rule        = "postgresql-tcp"
+      cidr_blocks = module.core_vpc.vpc_cidr_block
+    }
+  ]
+  egress_with_cidr_blocks = [
+    {
+      rule        = "all-all"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+}
---
+++ platform/definition/variables.tf
@@ -0,0 +1,8 @@
+variable "db_instance_class" {
+  default = {}
+}
+
+variable "all_db_instance_class" {
+  type    = string
+  default = "db.t2.medium"
+}
