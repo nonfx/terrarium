999ac9fd79149757544e11eff57148ae93adef18
Petr Malik <petr.malik@hotmail.com>
2023-08-24 07:45:59 -0400

    TER-199 Fail is module list file is empty (#55)
    Return an error if module file contains no items.

--- src/pkg/metadata/cli/model_test.go
+++ src/pkg/metadata/cli/model_test.go
@@ -1,6 +1,13 @@
 package cli
 
-import "testing"
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
+)
 
 func TestFarmModuleList_Validate(t *testing.T) {
 	tests := []struct {
@@ -72,6 +79,94 @@ func TestFarmModuleList_Validate(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := tt.list.Validate(); (err != nil) != tt.wantErr {
 				t.Errorf("FarmModuleList.Validate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestFarmModuleRef_WriteFile(t *testing.T) {
+	tests := []struct {
+		name         string
+		m            FarmModuleRef
+		wantDirPath  string
+		wantFilePath string
+		wantErr      bool
+	}{
+		{
+			name: "valid reference",
+			m: FarmModuleRef{
+				Name:    "Home",
+				Source:  "Carolina",
+				Version: "transmit",
+				Export:  true,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotDirPath, gotFilePath, err := tt.m.CreateTerraformFile()
+			assert.NoError(t, err)
+			assert.NotEmpty(t, gotFilePath)
+			assert.True(t, strings.HasPrefix(gotFilePath, gotDirPath))
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestLoadFarmModules(t *testing.T) {
+	tests := []struct {
+		name    string
+		list    FarmModuleList
+		wantErr bool
+	}{
+		{
+			name: "empty list",
+			list: FarmModuleList{
+				Farm: []FarmModuleRef{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "non-empty list",
+			list: FarmModuleList{
+				Farm: []FarmModuleRef{
+					{
+						Name:    "Wooden",
+						Source:  "Automotive",
+						Version: "cross-media",
+						Export:  true,
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := LoadFarmModules("8065c60d-f7f8-4a25-b000-1a7578ac665e")
+			assert.Error(t, err)
+
+			fp, err := os.CreateTemp("", "*")
+			if err != nil {
+				t.Fatal(err)
+			}
+			b, err := yaml.Marshal(tt.list)
+			if err != nil {
+				t.Fatal(err)
+			}
+			fp.Write(b)
+
+			got, err := LoadFarmModules(fp.Name())
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.list, got)
 			}
 		})
 	}
