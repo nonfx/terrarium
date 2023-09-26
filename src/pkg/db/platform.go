52048806461d1b01543a63eb43ecb77550c724f1
26christy <26christy@gmail.com>
2023-09-26 13:18:31 +0530

    TER-272 db schema

--- src/pkg/db/platform.go
+++ src/pkg/db/platform.go
@@ -8,14 +8,14 @@ import "github.com/google/uuid"
 type Platform struct {
 	Model
 
-	Name          string `gorm:"unique"`
+	Name          string
 	RepoURL       string
 	RepoDirectory string
-	CommitSHA     string
+	CommitSHA     string `gorm:"unique"`
 	RefLabel      string // can be tag/branch/commit that user wrote in the yaml. example v0.1 or main.
 	LabelType     int    // 1=branch, 2=tag, 3=commit
 
-	Components []PlatformComponents `gorm:"foreignKey:PlatformID"` // Attributes of the module
+	Components []PlatformComponents `gorm:"foreignKey:PlatformID"`
 }
 
 // insert a row in DB or in case of conflict in unique fields, update the existing record and set the existing record ID in the given object
