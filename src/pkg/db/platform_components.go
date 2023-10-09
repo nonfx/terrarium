dcd437a781017fb59534ba5931ae28bf6f27fb39
26christy <26christy@gmail.com>
2023-10-09 18:17:25 +0530

    TER-310 updated gorm for PlatformComponents

--- src/pkg/db/platform_components.go
+++ src/pkg/db/platform_components.go
@@ -5,18 +5,18 @@ package db
 
 import "github.com/google/uuid"
 
-type PlatformComponents struct {
+type PlatformComponent struct {
 	Model
 
 	PlatformID   uuid.UUID `gorm:"uniqueIndex:platform_components_unique"`
-	DependencyID uuid.UUID `gorm:"uniqueIndex:dependency_components_unique"`
+	DependencyID uuid.UUID `gorm:"uniqueIndex:platform_components_unique"`
 
 	Platform   Platform   `gorm:"foreignKey:PlatformID"`
 	Dependency Dependency `gorm:"foreignKey:DependencyID"`
 }
 
 // insert a row in DB or in case of conflict in unique fields, update the existing record and set existing record ID in the given object
-func (db *gDB) CreatePlatformComponents(p *PlatformComponents) (uuid.UUID, error) {
+func (db *gDB) CreatePlatformComponents(p *PlatformComponent) (uuid.UUID, error) {
 	id, _, _, err := createOrGetOrUpdate(db.g(), p, []string{"platform_id", "dependency_id"})
 	return id, err
 }
