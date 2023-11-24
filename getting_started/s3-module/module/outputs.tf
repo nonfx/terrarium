625bc9c44a92f31f6209f8ba04911a262c28e078
himanshuDevopsGuy <himanshu.uniyal@cldcvr.com>
2023-11-24 15:01:40 +0530

    modified

--- getting_started/s3-module/module/outputs.tf
+++ getting_started/s3-module/module/outputs.tf
@@ -1,6 +1,6 @@
 output "tr_component_s3_bucket_id" {
-    value = {for k, v in module.tr_component_s3_bucket_for_logs : k => v.s3_bucket_id }
+    value = {for k, v in module.tr_component_s3_bucket : k => v.s3_bucket_id }
 }
 output "tr_component_s3_bucket_arn" {
-    value = {for k, v in module.tr_component_s3_bucket_for_logs : k => v.s3_bucket_arn }
+    value = {for k, v in module.tr_component_s3_bucket : k => v.s3_bucket_arn }
 }
--- getting_started/s3-module/out.tf
+++
@@ -1,9 +0,0 @@
-output "tr_component_s3_bucket_id" {
-  description = "The name of the bucket."
-  value       = aws_s3_bucket.example.id
-}
-
-output "tr_component_s3_bucket_arn" {
-  description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
-  value       = aws_s3_bucket.example.arn
-}
